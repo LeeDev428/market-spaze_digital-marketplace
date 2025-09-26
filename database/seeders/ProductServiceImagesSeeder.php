d12b8e2361acadabf2bba2656d2e1a7e7d1fc2ad
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-09-26 17:10:18 +0800

    Create ProductServiceImagesSeeder.php

---
+++ database/seeders/ProductServiceImagesSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProductServiceImagesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
