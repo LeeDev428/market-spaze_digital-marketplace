961ff1d03f3f995680774770ed9e1295ed13572b
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-09-26 17:07:16 +0800

    Update VendorProductService.php

--- app/Models/VendorProductService.php
+++ app/Models/VendorProductService.php
@@ -15,15 +15,46 @@ class VendorProductService extends Model
         'vendor_store_id',
         'name',
         'description',
+        'category',
         'price_min',
         'price_max',
+        'duration_minutes',
+        'discount_percentage',
+        'is_popular',
+        'is_guaranteed',
+        'is_professional',
+        'rating',
+        'total_reviews',
+        'response_time',
+        'includes',
+        'requirements',
+        'has_warranty',
+        'warranty_days',
+        'pickup_available',
+        'delivery_available',
+        'emergency_service',
+        'special_instructions',
+        'slug',
+        'tags',
         'is_active'
     ];
 
     protected $casts = [
         'price_min' => 'decimal:2',
         'price_max' => 'decimal:2',
-        'is_active' => 'boolean'
+        'discount_percentage' => 'decimal:2',
+        'rating' => 'decimal:2',
+        'is_active' => 'boolean',
+        'is_popular' => 'boolean',
+        'is_guaranteed' => 'boolean',
+        'is_professional' => 'boolean',
+        'has_warranty' => 'boolean',
+        'pickup_available' => 'boolean',
+        'delivery_available' => 'boolean',
+        'emergency_service' => 'boolean',
+        'includes' => 'array',
+        'requirements' => 'array',
+        'tags' => 'array'
     ];
 
     // Relationships
