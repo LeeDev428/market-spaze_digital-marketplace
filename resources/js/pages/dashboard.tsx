35c8a51076bd517568747d932dd75f4dc3c84f7d
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-10-11 16:21:15 +0800

    Update dashboard.tsx

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -94,9 +94,9 @@ export default function Dashboard({
     };
 
     const formatCurrency = (amount: number) => {
-        return new Intl.NumberFormat('en-US', {
+        return new Intl.NumberFormat('en-PH', {
             style: 'currency',
-            currency: 'USD'
+            currency: 'PHP'
         }).format(amount);
     };
 
